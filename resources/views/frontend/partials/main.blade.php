33e8db0a1e130af2199bc562504281dc17f6d1a1
Rabia Rabia <rdoll1765@gmail.com>
2023-12-28 13:23:11 +0500

    fixed footer error on setting page

--- resources/views/frontend/partials/main.blade.php
+++ resources/views/frontend/partials/main.blade.php
@@ -22,6 +22,7 @@
     @yield('content')
     {{-- footer include --}}
     @include('frontend.partials.footer')
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
