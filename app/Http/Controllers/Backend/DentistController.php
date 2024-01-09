d1fad51c51e2f17ecce0fd527e74d9f98ae33e19
AhmadRaza1998 <148233084+AhmadRaza1998@users.noreply.github.com>
2024-01-09 03:49:14 -0800

    fixzes

--- app/Http/Controllers/Backend/DentistController.php
+++ app/Http/Controllers/Backend/DentistController.php
@@ -7,4 +7,52 @@ use Illuminate\Http\Request;
 
 class DentistController extends Controller
 {
+    
+    public function dashboard()
+    {
+
+        return view('Dentists.dashboard');
+    }
+    public function statistics()
+    {
+        return view('Dentists.statistics');
+    }
+    public function Inventory()
+    {
+
+        return view('Dentists.inventory');
+    }
+    public function orders()
+    {
+        return view('Dentists.order');
+    }
+    public function Patient_list()
+    {
+        return view('Dentists.patients');
+    }
+    public function calander()
+    {
+        return view('Dentists.calander');
+    }
+    public function assigned_tasks()
+    {
+        return view('Dentists.tasks');
+    }
+    public function invoicing()
+    {
+        return view('Dentists.invoicing');
+    }
+    public function setting()
+    {
+        return view('Dentists.setting');
+    }
+    public function update_order()
+    {
+        return view('Dentists.updateorder');
+    }
+
+    public function Patient_sheet()
+    {
+        return view('Dentists.patient_sheet');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DentalController;
+use App\Http\Controllers\backend\DentistController;
 use App\Http\Controllers\DentalidController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,18 +16,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('Clinics.fiche_patient');
+// })->name('home');
+
+// route::get('/dashboard', [DentalController::class, 'dashboard'])->name('dashboard');
+// route::get('/statistics', [DentalController::class, 'statistics'])->name('statistics');
+// route::get('/inventory', [DentalController::class, 'inventory'])->name('inventory');
+// route::get('/order', [DentalController::class, 'orders'])->name('order');
+// route::get('/Patients', [DentalController::class, 'Patient_list'])->name('Patients');
+// route::get('/calendar', [DentalController::class, 'calander'])->name('calander');
+// route::get('/tasks', [DentalController::class, 'assigned_tasks'])->name('tasks');
+// route::get('/invoicing', [DentalController::class, 'invoicing'])->name('invoicing');
+// route::get('/setting', [DentalController::class, 'setting'])->name('setting');
+// route::get('/update-order', [DentalController::class, 'update_order'])->name('update_order');
+// route::get('/Patient-sheet', [DentalController::class, 'Patient_sheet'])->name('Patient_sheet');
+
+
 Route::get('/', function () {
-    return view('frontend.fiche_patient');
+    return view('Dentists.fiche_patient');
 })->name('home');
 
-route::get('/dashboard', [DentalController::class, 'dashboard'])->name('dashboard');
-route::get('/statistics', [DentalController::class, 'statistics'])->name('statistics');
-route::get('/inventory', [DentalController::class, 'inventory'])->name('inventory');
-route::get('/order', [DentalController::class, 'orders'])->name('order');
-route::get('/Patients', [DentalController::class, 'Patient_list'])->name('Patients');
-route::get('/calendar', [DentalController::class, 'calander'])->name('calander');
-route::get('/tasks', [DentalController::class, 'assigned_tasks'])->name('tasks');
-route::get('/invoicing', [DentalController::class, 'invoicing'])->name('invoicing');
-route::get('/setting', [DentalController::class, 'setting'])->name('setting');
-route::get('/update-order', [DentalController::class, 'update_order'])->name('update_order');
-route::get('/Patient-sheet', [DentalController::class, 'Patient_sheet'])->name('Patient_sheet');
+route::get('/dashboard', [DentistController::class, 'dashboard'])->name('dashboard');
+route::get('/statistics', [DentistController::class, 'statistics'])->name('statistics');
+route::get('/inventory', [DentistController::class, 'inventory'])->name('inventory');
+route::get('/order', [DentistController::class, 'orders'])->name('order');
+route::get('/Patients', [DentistController::class, 'Patient_list'])->name('Patients');
+route::get('/calendar', [DentistController::class, 'calander'])->name('calander');
+route::get('/tasks', [DentistController::class, 'assigned_tasks'])->name('tasks');
+route::get('/invoicing', [DentistController::class, 'invoicing'])->name('invoicing');
+route::get('/setting', [DentistController::class, 'setting'])->name('setting');
+route::get('/update-order', [DentistController::class, 'update_order'])->name('update_order');
+route::get('/Patient-sheet', [DentistController::class, 'Patient_sheet'])->name('Patient_sheet');
